532633d2a33e0e9138c134d68f6972cb6a33d600
Joan Rotbard <jrotbard@virtualmindsoftware.com>
2015-03-22 15:44:22 -0300

    caca nueva

---
+++ Vennuit/elements/footer.php
@@ -0,0 +1,89 @@
+   <!--footer-->
+   <footer class="footer">
+      <section id="footer" class="footer-about">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-3">
+                  <div class="widget widget-brand">
+                     <div class="brand">
+                        <img alt="image" src="images/logo-light.png" width="197" height="165">
+                     </div>
+                     <div class="social">
+                        <a href="#" class="facebook">
+                           <i class="fa fa-facebook"></i>
+                        </a>
+                        <a href="#" class="twitter">
+                           <i class="fa fa-twitter"></i>
+                        </a>
+                        <a href="#" class="dribbble">
+                           <i class="fa fa-dribbble"></i>
+                        </a>
+                        <a href="#" class="email">
+                           <i class="fa fa-envelope"></i>
+                        </a>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-md-3">
+                  <div class="widget widget-open">
+                     <div class="widget-title">
+                        <h2>OPENING HOURS</h2>
+                     </div>
+                     <div class="widget-calendar">
+                        <p><span class="content-event">Sunday </span>: 12:00 noon - 22:00pm</p>
+                        <p><span class="content-event">Mon - Thur </span>: 9:00am - 22:00pm</p>
+                        <p><span class="content-event">Fri - Sat </span>: 9:00am - 23:00pm</p>
+                     </div>
+                     <div class="widget-happy">
+                        <span class="content-event">Happy Hour </span>: Enjoy discounted meals every Tuesday 11am - 12 noon this month of November.
+                     </div>
+                  </div>
+               </div>
+               <div class="col-md-3">
+                  <div class="widget widget-twitter">
+                     <div class="widget-title">
+                        <h2>OUR TWEETS</h2>
+                     </div>
+                     <div class="list-tiwtter" data-number="1" data-username="envato" data-modpath="twitter/index.html">
+                     </div>
+                  </div>
+               </div>
+               <div class="col-md-3">
+                  <div class="widget widget-letter">
+                     <div class="widget-title">
+                        <h2>NEWSLETTER</h2>
+                     </div>
+                     <p>
+                        Join our mailing list and be the first to know about upcoming events and special offers.
+                     </p>
+                     <div class="widget-bottom">
+                        <form method="post" action="#" id="subscribe-form" data-mailchimp="true" class="form-letter">
+                           <div id="subscribe-content">
+                              <div class="input">
+                                 <input type="text" id="subscribe-email" class="your-email" name="subscribe-email" placeholder="Your email">
+                              </div>
+                              <div class="button">
+                                 <button type="button" id="subscribe-button" class="submit subcrible" title="Subscribe now">submit</button>
+                              </div>
+                           </div>
+                           <div id="subscribe-msg"></div>
+                        </form>
+                     </div>
+                  </div>      
+               </div>
+            </div>
+         </div>
+      </section>
+      <section class="footer-copy">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-6">
+                  <span class="footer-left">Copyright 2015 &copy; Vennuit. All rights are reserved.</span>
+               </div>
+               <div class="col-md-6">
+                  <span class="footer-right">Vennuit</span>
+               </div>
+            </div>
+         </div>
+      </section><!--/.footer-section-->
+   </footer>
